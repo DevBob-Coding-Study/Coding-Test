71ccfbb58b86aa95bdb6de92af4e64fce1fddf3f
jukrap <jukrap628@gmail.com>
2024-05-29 19:41:13 +0900

    1주차 / [PGS] 완주하지 못한 선수 / 50m

--- 1주차/PGS_완주하지 못한 선수/jucheol.js
+++ 1주차/PGS_완주하지 못한 선수/jucheol.js
@@ -1,4 +1,4 @@
-// 2차 해결 - 해시 사용
+// 4차 시도 (성공) - 해시 사용
 function solution(participant, completion) {   
     let hashMap = new Map();
     
@@ -19,42 +19,42 @@ function solution(participant, completion) {
 
 
 
-// 2차 해결 - 해시 사용
-// function solution(participant, completion) {   
-//     let hashMap = new Map();
-//     for(let i = 0;i<=participant.length;i++) {
-//         let par = participant[i];
-//         let com = completion[i];
-//         hashMap.set(par, (hashMap.get(par) || 0) + 1);
-//         hashMap.set(com, (hashMap.get(com) || 0) - 1);
-//     }
-//     for(let [i, j] of hashMap) {
-//         if(j > 0) {
-//             return i;
-//         }
-//     }
-// }
+// 3차 시도 (성공) - 해시 사용
+function solution(participant, completion) {   
+    let hashMap = new Map();
+    for(let i = 0;i<=participant.length;i++) {
+        let par = participant[i];
+        let com = completion[i];
+        hashMap.set(par, (hashMap.get(par) || 0) + 1);
+        hashMap.set(com, (hashMap.get(com) || 0) - 1);
+    }
+    for(let [i, j] of hashMap) {
+        if(j > 0) {
+            return i;
+        }
+    }
+}
 
 
 
-// 1차 해결 - 해시 미사용
-// function solution(participant, completion) {
-//     participant.sort();
-//     completion.sort();
+// 2차 시도 (성공) - 해시 미사용
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
     
-//     for(let i = 0;i<=participant.length;i++) {
-//         if(participant[i] != completion[i])
-//             return participant[i];
-//     }
-// }
+    for(let i = 0;i<=participant.length;i++) {
+        if(participant[i] != completion[i])
+            return participant[i];
+    }
+}
 
 
 
-// 실패 - Set 사용
-// function solution(participant, completion) {
-//     let participantSet = new Set(participant);
-//     let completionSet = new Set(completion);
+// 1차 시도 (실패) - Set 사용
+function solution(participant, completion) {
+    let participantSet = new Set(participant);
+    let completionSet = new Set(completion);
     
-//     let result = participant.filter((value) => !completionSet.has(value))
-//     console.log(result);
-// }
+    let result = participant.filter((value) => !completionSet.has(value))
+    console.log(result);
+}
