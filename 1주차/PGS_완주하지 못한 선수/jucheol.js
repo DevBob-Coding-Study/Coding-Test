14fb21f55f54dc2008330555dbb5d533e20aa94c
jukrap <jukrap628@gmail.com>
2024-05-29 17:21:36 +0900

    1주차 / [PGS] 완주하지 못한 선수 / 50m

---
+++ 1주차/PGS_완주하지 못한 선수/jucheol.js
@@ -0,0 +1,60 @@
+// 2차 해결 - 해시 사용
+function solution(participant, completion) {   
+    let hashMap = new Map();
+    
+    for (const par of participant) {
+        hashMap.set(par, (hashMap.get(par) || 0) + 1);
+    }
+    
+    for (const com of completion) {
+        hashMap.set(com, hashMap.get(com) - 1);
+    }
+    
+    for(let [name, count] of hashMap) {
+        if(count > 0) {
+            return name;
+        }
+    }
+}
+
+
+
+// 2차 해결 - 해시 사용
+// function solution(participant, completion) {   
+//     let hashMap = new Map();
+//     for(let i = 0;i<=participant.length;i++) {
+//         let par = participant[i];
+//         let com = completion[i];
+//         hashMap.set(par, (hashMap.get(par) || 0) + 1);
+//         hashMap.set(com, (hashMap.get(com) || 0) - 1);
+//     }
+//     for(let [i, j] of hashMap) {
+//         if(j > 0) {
+//             return i;
+//         }
+//     }
+// }
+
+
+
+// 1차 해결 - 해시 미사용
+// function solution(participant, completion) {
+//     participant.sort();
+//     completion.sort();
+    
+//     for(let i = 0;i<=participant.length;i++) {
+//         if(participant[i] != completion[i])
+//             return participant[i];
+//     }
+// }
+
+
+
+// 실패 - Set 사용
+// function solution(participant, completion) {
+//     let participantSet = new Set(participant);
+//     let completionSet = new Set(completion);
+    
+//     let result = participant.filter((value) => !completionSet.has(value))
+//     console.log(result);
+// }
